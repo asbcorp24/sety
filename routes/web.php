9fb4b1e506ffb3132a623925abd921f7796e6fcd
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-08-17 20:46:26 +0300

    feat: add Windows console practice routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TopicController;
+use App\Http\Controllers\WindowsConsolePracticeController;
 use App\Http\Controllers\Admin\UserController as AdminUserController;
 use App\Http\Controllers\Admin\TopicController as AdminTopicController;
 use Illuminate\Support\Facades\Route;
@@ -10,4 +11,8 @@ Route::middleware('guest')->group(function(){ Route::get('/login',[AuthControlle
 Route::post('/logout',[AuthController::class,'logout'])->middleware('auth')->name('logout');
 Route::get('/topics',[TopicController::class,'index'])->name('topics.index');
 Route::get('/topics/{topic}',[TopicController::class,'show'])->name('topics.show');
+Route::middleware('auth')->group(function(){
+ Route::get('/practice/windows-command-line',[WindowsConsolePracticeController::class,'show'])->name('practice.windows');
+ Route::post('/practice/windows-command-line/result',[WindowsConsolePracticeController::class,'store'])->name('practice.windows.result');
+});
 Route::prefix('admin')->middleware(['auth','admin'])->name('admin.')->group(function(){ Route::get('/',[AdminController::class,'index'])->name('index'); Route::get('/users',fn()=>view('admin.users'))->name('users'); Route::get('/topics',fn()=>view('admin.topics'))->name('topics'); Route::get('/ajax/users',[AdminUserController::class,'index']); Route::post('/ajax/users',[AdminUserController::class,'store']); Route::put('/ajax/users/{user}',[AdminUserController::class,'update']); Route::delete('/ajax/users/{user}',[AdminUserController::class,'destroy']); Route::get('/ajax/topics',[AdminTopicController::class,'index']); Route::post('/ajax/topics',[AdminTopicController::class,'store']); Route::put('/ajax/topics/{topic}',[AdminTopicController::class,'update']); Route::delete('/ajax/topics/{topic}',[AdminTopicController::class,'destroy']); });
